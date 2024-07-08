f9af1f5bb6393afa7e302b0b4c47fdd4938d7f38
narendrankt <naren.cse008@gmail.com>
2024-07-08 07:05:26 +0530

    update file structure and sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-   return <nav className="bg-dark-1 h-[72px]">navbar</nav>;
+   return (
+      <nav className="bg-dark-1 h-[72px] text-white flex-between fixed w-full z-50 px-6 py-4 lg:px-10">
+         <Link href="/" className="flex items-center gap-1">
+            <Image src={"/icons/logo.svg"} alt="Logo" height={32} width={32} className=" max-sm:size-10" />
+            <p className="text-white text-[26px] font-extrabold max-sm:hidden">Xoom</p>
+         </Link>
+      </nav>
+   );
 };
 
 export default Navbar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,33 @@
+"use client";
+import {sidebarLinks} from "@/constants";
+import {cn} from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
 import React from "react";
 
 const Sidebar = () => {
+   const pathName = usePathname();
    return (
       <section className="sticky left-0 top-0 h-screen flex flex-col w-fit justify-between bg-dark-1 p-6 pt-28 text-white max-md:hidden lg:w-[264px]">
-         <div>s</div>
+         <div className="flex flex-col gap-6">
+            {sidebarLinks.map((link) => {
+               const isActive = pathName.startsWith(link.route) || pathName === link.route;
+
+               return (
+                  <Link
+                     href={link.route}
+                     key={link.label}
+                     className={cn("flex gap-4 items-center p-4 rounded-lg justify-start", {
+                        "bg-blue-1": isActive,
+                     })}
+                  >
+                     <Image src={link.imgUrl} alt={link.label} width={24} height={24} />
+                     <p className="text-lg font-semibold max-lg:hidden">{link.label}</p>
+                  </Link>
+               );
+            })}
+         </div>
       </section>
    );
 };
